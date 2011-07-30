5d64ea7e20b7694e663cc4971eb7f1fc11781534
Damon Wang <damon.wang@gmail.com>
2011-07-30 12:51:10 -0500

    bugfix: zero field names

--- record/record.py
+++ record/record.py
@@ -9,7 +9,7 @@ class {typename} (object):
 
     immutable_form = namedtuple('Immutable{typename}', __slots__)
 
-    def __init__(self, {fields_as_args}):
+    def __init__(self{fields_as_args}):
         {init}
     def __repr__(self):
         'return a nicely formatted representation string'
@@ -23,7 +23,7 @@ class {typename} (object):
         'return an immutable copy using collections.namedtuple.'
         return self.immutable_form({field_values})
 
-    def but_with(self, {fields_as_args}):
+    def but_with(self{fields_as_args}):
         \'\'\'
         a functional update: return a new record, filling in unspecified
         fields with the corresponding values in this record.
@@ -32,27 +32,54 @@ class {typename} (object):
 '''
 
 def specialize_template(typename, field_names):
-    return class_template.format(typename=typename,
-        fields = repr(tuple(field_names)),
-        fields_as_args = ', '.join('%s=None' % field for field in field_names),
-        # this is an odd syntax, but necessary because making the
-        # indentation work out across multiple lines is difficult
-        # (This is clearly a limitation of the technique that will
-        # become more of a problem if we try to generalize it into a
-        # macro system.)
-        init = '%s = %s' % (
-          ', '.join('self.%s' % field for field in field_names),
-          ', '.join('%s' % field for field in field_names)),
-        repr_format = '{typename}({values})'.format(
-          typename=typename,
-          values = ', '.join('%s={%s}' % (field, field) 
-            for field in field_names)),
-          field_values = ', '.join('%s=self.%s' % (field, field)
-            for field in field_names),
-          field_values_as_defaults = 
-          ', '.join('{field} = {field} or self.{field}'.format(field=field)
-            for field in field_names)
-          )
+  if len(field_names) > 0:
+    # hence the strange "self{fields_as_args syntax in the template
+    fields_as_args = ', ' + ', '.join('%s=None' % field for field in field_names)
+    # this strange tuple assignment syntax is but necessary because
+    # making the indentation work out across multiple lines is difficult
+    # (This is clearly a limitation of the technique that will
+    # become more of a problem if we try to generalize it into a
+    # macro system.)
+    init = '%s = %s' % (
+      ', '.join('self.%s' % field for field in field_names),
+      ', '.join('%s' % field for field in field_names))
+  else: fields_as_args, init = '', 'pass'
+
+  return class_template.format(typename=typename,
+      fields = repr(tuple(field_names)),
+      fields_as_args = fields_as_args,
+      init = init,
+      repr_format = '{typename}({values})'.format(
+        typename=typename,
+        values = ', '.join('%s={%s}' % (field, field) 
+          for field in field_names)),
+        field_values = ', '.join('%s=self.%s' % (field, field)
+          for field in field_names),
+        field_values_as_defaults = 
+        ', '.join('{field} = {field} or self.{field}'.format(field=field)
+          for field in field_names)
+        )
+
+def unsafe_define(typename, field_names):
+    class_definition = specialize_template(typename, field_names)
+    namespace = dict(__name__='Record_%s' % typename)
+
+    # the following lines are closely copied from collections.namedtuple
+    try: exec(class_definition, namespace)
+    except SyntaxError as e:
+        raise SyntaxError(e.msg + ':\n\n' + class_definition)
+    result = namespace[typename]
+
+    # For pickling to work, the __module__ variable needs to be set to the
+    # frame
+    # where the named tuple is created. Bypass this step in enviroments where
+    # sys._getframe is not defined (Jython for example) or sys._getframe is not
+    # defined for arguments greater than 0 (IronPython).
+    try: frame_globals = sys._getframe(1).f_globals
+    except (AttributeError, ValueError): pass
+    result.__module__ = frame_globals.get('__name__', '__main__')
+
+    return result
 
 class Record (object):
   '''
@@ -67,22 +94,4 @@ class Record (object):
 
   @staticmethod
   def define(typename, field_names):
-      class_definition = specialize_template(typename, field_names)
-      namespace = dict(__name__='Record_%s' % typename)
-
-      # the following lines are closely copied from collections.namedtuple
-      try: exec(class_definition, namespace)
-      except SyntaxError as e:
-          raise SyntaxError(e.msg + ':\n\n' + class_definition)
-      result = namespace[typename]
-
-      # For pickling to work, the __module__ variable needs to be set to the
-      # frame
-      # where the named tuple is created. Bypass this step in enviroments where
-      # sys._getframe is not defined (Jython for example) or sys._getframe is not
-      # defined for arguments greater than 0 (IronPython).
-      try: frame_globals = sys._getframe(1).f_globals
-      except (AttributeError, ValueError): pass
-      result.__module__ = frame_globals.get('__name__', '__main__')
-
-      return result
+    return unsafe_define(typename, field_names)
--- record/test.py
+++ record/test.py
@@ -1,79 +1,92 @@
 import unittest 
+import record
 from record import Record
 
 class TestFoo(unittest.TestCase):
-    def setUp(self):
-        record = Record.define('Foo', ('foo', 'bar', 'baz'))
-        self.foo = record(foo=1, bar=2, baz=3)
+  def setUp(self):
+    record = Record.define('Foo', ('foo', 'bar', 'baz'))
+    self.foo = record(foo=1, bar=2, baz=3)
 
-    def test_init(self):
-        foo = self.foo
-        self.assert_(foo.foo == 1)
-        self.assert_(foo.bar == 2)
-        self.assert_(foo.baz == 3)
+  def test_init(self):
+    'initialize and retrieve'
+    foo = self.foo
+    self.assert_(foo.foo == 1)
+    self.assert_(foo.bar == 2)
+    self.assert_(foo.baz == 3)
 
-    def test_repr(self):
-        self.assert_(repr(self.foo) == 'Foo(foo=1, bar=2, baz=3)')
+  def test_zero(self):
+    'zero fields'
+    self.assert_(Record.define('Foo', ()))
 
-    def test_as_dict(self):
-        foo = self.foo
-        self.assert_(foo.as_dict() 
-                == dict(foo=foo.foo, bar=foo.bar, baz=foo.baz))
+  def test_repr(self):
+    'repr'
+    self.assert_(repr(self.foo) == 'Foo(foo=1, bar=2, baz=3)')
 
-    def test_as_namedtuple(self):
-        foo = self.foo
-        ntup = foo.as_namedtuple()
-        self.assert_(ntup == (foo.foo, foo.bar, foo.baz))
-        self.assert_(ntup.foo == foo.foo)
-        self.assert_(ntup.bar == foo.bar)
-        self.assert_(ntup.baz == foo.baz)
+  def test_as_dict(self):
+    'as_dict'
+    foo = self.foo
+    self.assert_(foo.as_dict() 
+        == dict(foo=foo.foo, bar=foo.bar, baz=foo.baz))
 
-    def test_but_with(self):
-        new_foo = self.foo.but_with(bar='2', baz='3')
-        self.assert_(new_foo.bar == '2')
-        self.assert_(new_foo.baz == '3')
-        self.assert_(new_foo.foo == self.foo.foo)
+  def test_as_namedtuple(self):
+    'as_namedtuple'
+    foo = self.foo
+    ntup = foo.as_namedtuple()
+    self.assert_(ntup == (foo.foo, foo.bar, foo.baz))
+    self.assert_(ntup.foo == foo.foo)
+    self.assert_(ntup.bar == foo.bar)
+    self.assert_(ntup.baz == foo.baz)
+
+  def test_but_with(self):
+    'but_with'
+    new_foo = self.foo.but_with(bar='2', baz='3')
+    self.assert_(new_foo.bar == '2')
+    self.assert_(new_foo.baz == '3')
+    self.assert_(new_foo.foo == self.foo.foo)
 
 class TestHam(unittest.TestCase):
-    '''
-    this test suite ensures that I've generalized all the specific
-    references to foo, bar, baz etc. from when I wrote out the template
-    manually.
-    '''
-    def setUp(self):
-        record = Record.define('Ham', ('ham', 'spam', 'eggs'))
-        self.ham = record(ham=1, spam=2, eggs=3)
+  '''
+  this test suite ensures that I've generalized all the specific
+  references to foo, bar, baz etc. from when I wrote out the template
+  manually.
+  '''
+  def setUp(self):
+    record = Record.define('Ham', ('ham', 'spam', 'eggs'))
+    self.ham = record(ham=1, spam=2, eggs=3)
 
-    def test_init(self):
-        ham = self.ham
-        self.assert_(ham.ham == 1)
-        self.assert_(ham.spam == 2)
-        self.assert_(ham.eggs == 3)
+  def test_init(self):
+    'initialize and retrieve'
+    ham = self.ham
+    self.assert_(ham.ham == 1)
+    self.assert_(ham.spam == 2)
+    self.assert_(ham.eggs == 3)
 
-    def test_repr(self):
-        self.assert_(repr(self.ham) == 'Ham(ham=1, spam=2, eggs=3)')
+  def test_repr(self):
+    'repr'
+    self.assert_(repr(self.ham) == 'Ham(ham=1, spam=2, eggs=3)')
 
-    def test_as_dict(self):
-        ham = self.ham
-        self.assert_(ham.as_dict() 
-                == dict(ham=ham.ham, spam=ham.spam, eggs=ham.eggs))
+  def test_as_dict(self):
+    'as_dict'
+    ham = self.ham
+    self.assert_(ham.as_dict() 
+        == dict(ham=ham.ham, spam=ham.spam, eggs=ham.eggs))
 
-    def test_as_namedtuple(self):
-        ham = self.ham
-        ntup = ham.as_namedtuple()
-        self.assert_(ntup == (ham.ham, ham.spam, ham.eggs))
-        self.assert_(ntup.ham == ham.ham)
-        self.assert_(ntup.spam == ham.spam)
-        self.assert_(ntup.eggs == ham.eggs)
+  def test_as_namedtuple(self):
+    ham = self.ham
+    ntup = ham.as_namedtuple()
+    self.assert_(ntup == (ham.ham, ham.spam, ham.eggs))
+    self.assert_(ntup.ham == ham.ham)
+    self.assert_(ntup.spam == ham.spam)
+    self.assert_(ntup.eggs == ham.eggs)
 
-    def test_but_with(self):
-        new_ham = self.ham.but_with(spam='2', eggs='3')
-        self.assert_(new_ham.spam == '2')
-        self.assert_(new_ham.eggs == '3')
-        self.assert_(new_ham.ham == self.ham.ham)
+  def test_but_with(self):
+    new_ham = self.ham.but_with(spam='2', eggs='3')
+    self.assert_(new_ham.spam == '2')
+    self.assert_(new_ham.eggs == '3')
+    self.assert_(new_ham.ham == self.ham.ham)
 
 def test():
-    loader = unittest.TestLoader()
-    unittest.TextTestRunner().run(
-        unittest.TestSuite(map(loader.loadTestsFromTestCase,
-          (TestFoo, TestHam))))
+  loader = unittest.TestLoader()
+  unittest.TextTestRunner().run(
+      unittest.TestSuite(map(loader.loadTestsFromTestCase,
+        (TestFoo, TestHam))))
